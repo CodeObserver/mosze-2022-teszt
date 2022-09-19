a3c70f6b8b1b754296129474deb35084d693e860
Göntér Mátyás <71329049+CodeObserver@users.noreply.github.com>
2022-09-19 18:29:34 +0200

    Hibák kijavítva

--- main.cpp
+++ main.cpp
@@ -4,21 +4,19 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS];  // Az 'N_ELEMENTS'  'NELEMENTS'-nek volt írva
+    std::cout << "1-100 ertekek duplazasa"<<std::endl;  // ' van használva " helyett, valamint sortörés
+    for (int i = 1; i <= N_ELEMENTS; i++) //itt kikell egészíteni a forciklust, mert hiányoznak kritériumok
     {
         b[i] = i * 2;
+        std::cout << "Ertek:" << b[i] <<std::endl; // Az egyik forciklust eltüntettem , mert szükségtelen volt, valamint hiányzik innen egy ; és egy sortorés
     }
-    for (int i = 0; i; i++)
-    {
-        std::cout << "Ertek:"
-    }    
+  
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag=0; //változót inicializálni kell
+    for (int i = 0; i <= N_ELEMENTS; i++) //N_ELEMENTS után nem , kell hanem ; kell, valamint az egyenlőség is megengedett
     {
-        atlag += b[i]
+        atlag += b[i]; //hiányzik a ; a végéről
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
